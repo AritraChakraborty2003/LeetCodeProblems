cd7a8f92786f1b2b2b07bf4f28e04b032d11e2f3
Aritra Chakraborty <107548404+AritraChakraborty2003@users.noreply.github.com>
2024-02-17 18:15:20 +0530

    Time: 5 ms (39.49%) | Memory: 6.4 MB (66.30%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.c
+++ 206-reverse-linked-list/reverse-linked-list.c
@@ -5,17 +5,17 @@
  *     struct ListNode *next;
  * };
  */
-struct ListNode* reverseList(struct ListNode* h) {
+struct ListNode* reverseList(struct ListNode* head) {
+    
     struct ListNode* prev=NULL;
-	struct ListNode* next=NULL;
-	struct ListNode* curr=h;
-	while(curr!=NULL){
-		next=curr->next;
-		curr->next=prev;
-		prev=curr;
-		curr=next;
-
-	}
-	h=prev;
-    return h;
+    struct ListNode* next=NULL;
+    struct ListNode* curr=head;
+    while(curr!=NULL){
+        next=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=next;
+   }
+   head=prev;
+   return head;
 }
