faa078c0a0b3f16b7b05166b4dab7ddf7cf2eee6
Aritra Chakraborty <107548404+AritraChakraborty2003@users.noreply.github.com>
2024-10-19 18:07:02 +0530

    Time: 4 ms (100.00%) | Memory: 44.2 MB (27.72%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x<0)
-            return false;
-        int cpy=x; int res=0;
-        while(cpy!=0){
-            int rem=cpy%10;
-            res=res*10+rem;
-            cpy/=10;
+        int copyValue=x;
+        int palindromeValue=0;
+        while(x>0){
+            int rem=x%10;
+            palindromeValue=palindromeValue*10+rem;
+            x=x/10;
         }
 
-        if(res==x) return true;
+        if(palindromeValue==copyValue) return true;
+
         return false;
-    }   
+    }
 }
