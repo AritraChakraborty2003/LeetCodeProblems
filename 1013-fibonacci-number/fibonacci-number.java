9529b3bfce9f4e4e653471ab07751ba668f2018f
Aritra Chakraborty <107548404+AritraChakraborty2003@users.noreply.github.com>
2024-10-19 19:34:29 +0530

    Time: 9 ms (27.05%) | Memory: 40.4 MB (28.55%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,8 +1,13 @@
 class Solution {
     public int fib(int n) {
-       if(n<=1)
+      if(n<0){
+        return 0;
+      }   
+      if(n<=1){
         return n;
-       else
+      }
+      else{
         return fib(n-1)+fib(n-2);
+      }
     }
 }
